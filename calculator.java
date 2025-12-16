c39eba62186a1bb159160613bf0e75b25cd614b6
ganeshreddy1524 <ganeshreddykandi@gmail.com>
2025-12-16 18:37:16 +0530

    methods added

--- calculator.java
+++ calculator.java
@@ -1,7 +1,12 @@
 class Calculator{
 	public static void main(String args[]){
 	
-
+	int add(int a,int b){
+		return a+b;
+	}
+	int sub(int a,int b){
+		return a-b;
+	}
 	int divide(int a,int b){
 		return a/b;
 
